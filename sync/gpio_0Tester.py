e7f61b765dfad63902447da0b56af495e3ea4b9a
magdesign <info@magdesign.ch>
2016-02-16 13:29:42 +0100

    extension board test files

--- sync/gpio_0Tester.py
+++ sync/gpio_0Tester.py
@@ -4,15 +4,37 @@ import RPi.GPIO as GPIO
 GPIO.setmode (GPIO.BOARD)
 GPIO.setwarnings (False)
 
+#Outputs
+GPIO.setup(29, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
 
-GPIO.setup (7,GPIO.IN)
-GPIO.setup (11,GPIO.IN)
-GPIO.setup (12,GPIO.IN)
-GPIO.setup (13,GPIO.IN)
-GPIO.setup (15,GPIO.IN)
-GPIO.setup (16,GPIO.IN)
-GPIO.setup (18,GPIO.IN)
-GPIO.setup (22,GPIO.IN)
+#LED
+GPIO.setup(36, GPIO.OUT)
+
+#buttons onboard
+GPIO.setup(35, GPIO.IN)
+GPIO.setup(38, GPIO.IN)
+GPIO.setup(40, GPIO.IN)
+GPIO.setup(37, GPIO.IN)
+
+#inputs external
+GPIO.setup(33, GPIO.IN)
+GPIO.setup(32, GPIO.IN)
+
+#test power LED for 2 seconds
+GPIO.output(36, GPIO.HIGH)
+sleep (2)
+GPIO.output(36, GPIO.LOW)
+
+#test Output 1 for 2 seconds
+GPIO.output(29, GPIO.HIGH)
+sleep (2)
+GPIO.output(29, GPIO.LOW)
+
+#test Output 2 for 2 seconds
+GPIO.output(31, GPIO.HIGH)
+sleep (2)
+GPIO.output(31, GPIO.LOW)
 
 
 
@@ -20,22 +42,18 @@ GPIO.setup (22,GPIO.IN)
 
 
 while(True):
-    if (GPIO.input(7)):
-        print ("Taste 4")
-    if (GPIO.input(11)):
-        print ("Taste 3")
-    if (GPIO.input(12)):
-        print ("Taste 2")
-    if (GPIO.input(13)):
-        print ("Taste 1")
-    if (GPIO.input(15)):
-        print ("Taste 8")
-    if (GPIO.input(16)):
-        print ("Taste 7")
-    if (GPIO.input(18)):
-        print ("Taste 6")
-    if (GPIO.input(22)):
-        print ("Taste 5")
+    if (GPIO.input(32)==GPIO.LOW):
+        print ("IN 1")
+    if (GPIO.input(33)==GPIO.LOW):
+        print ("IN 2")
+    if (GPIO.input(35)):
+        print ("Button 1")
+    if (GPIO.input(38)):
+        print ("Button 2")
+    if (GPIO.input(40)):
+        print ("Button 3")
+    if (GPIO.input(37)):
+        print ("Button 4")
 
 
 GPIO.cleanup()
